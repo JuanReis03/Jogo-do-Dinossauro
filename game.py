f917ee22b55aba6613f6894f6b38cf0374d34457
Juan <juanreis07@gmail.com>
2023-11-22 20:55:27 -0300

    Adicionando pontuação e recorde

--- game.py
+++ game.py
@@ -17,16 +17,27 @@ class Game(BaseImage):
         self.mensagemIniciar = Label(Game.mensagemIniciar, 200, 200)
         self.obstaculo = Obstaculo()
         self._contador = Contador(5)
+        self.pontuacao = 0
+        self.recorde = 0
+        self.mensagemPontuacao = Label(f'Pontos: {self.pontuacao}', 20, 20)
+        self.mensagemRecorde = Label(f'Recorde: self.recorde', 20, 50)
 
     def update(self):
         if (not Status.executando and keyboard.is_key_just_down('space')):
             self.mensagemIniciar._hide()
             self.iniciar()
+            
 
         if (Status.executando):
             if (self.deve_spawnar_obstaculo()):
                 self.obstaculo.deve_spawnar = True
                 # self.obstaculo = random.choice([Passaro(True), Cacto(True)])
+            self.pontuacao += 1
+            self.mensagemPontuacao.text = f'Pontos: {self.pontuacao}'
+            
+            if self.pontuacao > self.recorde:
+                self.recorde = self.pontuacao
+                self.mensagemRecorde.text = f'Recorde: {self.recorde}'
 
     def deve_spawnar_obstaculo(self):
         if self._contador.esta_zerado():
@@ -42,4 +53,9 @@ class Game(BaseImage):
         Status.executando = True
         self.ground.animar()
         self.ground2.animar()
+        if self.recorde == 0:
+            self.mensagemRecorde.text = f'Recorde: {self.recorde}'
+        if self.pontuacao > self.recorde:
+            self.recorde = self.pontuacao
+            self.mensagemRecorde.text = f'Recorde: {self.recorde}'
 
